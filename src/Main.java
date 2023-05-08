bed119d6a123b4e9abe45ab33fe0da2f9c6b7ec2
Gustavo O <gugagp05003@gmail.com>
2023-05-08 11:56:52 -0300

    Apaguei tudo pro novo conteúdo

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        //Aula inicial de Orientação à Objetos
 
-        System.out.println("Salve mundo!");
-// Laço enquanto - while
-        int contador = 0;
-        while(contador < 10){
-//processos
-            System.out.println("Contador = " + contador);
-            contador = contador + 1;//incremento
-        }
-// Laço faça enquanto - do while
-        int cont = 1;
-        do{
-            System.out.println(cont);
-            cont++; //incremento
-        }while(cont <= 10);
-
-// Laço para faça for
-        for(int x=0; x<10;x++){
-            System.out.println(x);
-        }
     }
 }
