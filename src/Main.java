1d7344beb2d1e721b1e874476a774752bc00526f
Gustavo O <gugagp05003@gmail.com>
2023-05-08 11:45:55 -0300

    Laços de repetição

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
 public class Main {
     public static void main(String[] args) {
+// Laço enquanto - while
+        int contador = 0;
+        while(contador < 10){
+//processos
+            System.out.println("Contador = " + contador);
+            contador = contador + 1;//incremento
+        }
+// Laço faça enquanto - do while
+        int cont = 1;
+        do{
+            System.out.println(cont);
+            cont++; //incremento
+        }while(cont <= 10);
 
-
-        System.out.println("Hello world!");
+// Laço para faça for
+        for(int x=0; x<10;x++){
+            System.out.println(x);
+        }
     }
 }
