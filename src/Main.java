a61525ad13a9382dfaedba1397dd9d0375e21055
Gustavo O <gugagp05003@gmail.com>
2023-05-08 11:53:11 -0300

    Laços de repetição e salve mundo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("Salve mundo!");
 // Laço enquanto - while
         int contador = 0;
         while(contador < 10){
