0c7e52f8046698e1754ae2e768c7a1658e2b0bf4
Gustavo O <gugagp05003@gmail.com>
2023-05-22 12:32:23 -0300

    Um monte de coisa sobre objetos e os krl muito termo dahora que eu não vou lembrar

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,19 @@ public class Main {
     public static void main(String[] args) {
         //Aula inicial de Orientação à Objetos
         Pessoa adao = new Pessoa();
+        //Declaração
+        Pessoa qualquer;
+        //Instanciação
+        qualquer = new Pessoa();
+
+        //definir forma do objeto
+        qualquer.nome = "Rita";
+        qualquer.sobrenome = "Lee";
+        //Definir comportamento
+        qualquer.falar();
+        System.out.println(qualquer.falar("alto"));
+
+
+
     }
 }
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -3,5 +3,20 @@ public class Pessoa {
     String nome;
     String sobrenome;
 
+    //Métodos
+    public void falar() {
+        System.out.println("Falei");
+    }
+    //sobrecarga do método falar()
+    public String falar(String volume){
+        return "Falei " + volume;
+    }
+    //Construtor
+    Pessoa(){
+
+    }
+    Pessoa(String _nome){
+        this.nome = _nome;
+    }
 
 }
